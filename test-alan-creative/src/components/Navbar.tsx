f1de67312c536bcddf6fdc89c5f0a65e2a91656c
Adhista Dera <deradhista@gmail.com>
2023-03-06 02:17:55 +0700

    style: repair tab menu

--- test-alan-creative/src/components/Navbar.tsx
+++ test-alan-creative/src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export const Header = () => {
   return (
     <div className="navbar bg-[#00ACEE] w-full">
@@ -11,8 +13,12 @@ export const Header = () => {
 export const Menu = () => {
   return (
     <div className="tabs flex justify-start w-full shadow-lg px-10 pt-5 space-x-5">
-      <a className="tab tab-bordered">Food</a>
-      <a className="tab tab-bordered tab-active">Transaksi</a>
+      <Link to="/food" className="tab tab-bordered">
+        Food
+      </Link>
+      <Link to="/" className="tab tab-bordered">
+        Transaksi
+      </Link>
     </div>
   );
 };
